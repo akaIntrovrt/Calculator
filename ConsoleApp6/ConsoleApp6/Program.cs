674c97f064090221cb5292c5043faf0cbe4f9e95
itought <mountyjapan24@gmail.com>
2025-10-02 14:12:01 +0300

    user input_v2

--- ConsoleApp6/ConsoleApp6/Program.cs
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -2,7 +2,7 @@
 int num = int.Parse(Console.ReadLine());
 Console.Write("Enter number -> ");
 int num2 = int.Parse(Console.ReadLine());
-Console.Write("Enter 1 - to *, 2 to +, 3 - to '-' -> ");
+Console.Write("Enter 1 - to * | 2 to + | 3 - to '-' | 4 to % -> ");
 int result = int.Parse(Console.ReadLine());
 
 if (result == 1)
@@ -15,4 +15,8 @@ else if(result == 2)
      int r = num + num2;
      Console.WriteLine(r);
 }
-
+else if (result == 3)
+{
+    int r = num - num2;
+    Console.WriteLine(r);
+}
