e555eb1a725b4c93d525aeae5d99af58165a8b2c
itought <mountyjapan24@gmail.com>
2025-10-02 14:09:34 +0300

    user input

--- ConsoleApp6/ConsoleApp6/Program.cs
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -1,2 +1,13 @@
 ﻿Console.Write("Enter number -> ");
 int num = int.Parse(Console.ReadLine());
+Console.Write("Enter number -> ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Enter 1 - to *, 2 to +, 3 - to '-' -> ");
+int result = int.Parse(Console.ReadLine());
+
+if (result == 1)
+{
+    int r = num * num2;
+    Console.WriteLine(r);
+}
+
