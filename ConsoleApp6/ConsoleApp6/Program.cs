8560662d5b9c19d36a403bf56c22b18912088084
itought <mountyjapan24@gmail.com>
2025-10-06 12:57:34 +0300

    Update Program.cs

--- ConsoleApp6/ConsoleApp6/Program.cs
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -1,27 +1,47 @@
-﻿Console.Write("Enter number -> ");
-int num = int.Parse(Console.ReadLine());
-Console.Write("Enter number -> ");
-int num2 = int.Parse(Console.ReadLine());
-Console.Write("Enter \n| 1 - to * \n| 2 to + \n| 3 - to '-' \n| 4 to % -> ");
-int result = int.Parse(Console.ReadLine());
+﻿Console.Write("Введите оценку студента с номером '1' -> ");
+int student1 = int.Parse(Console.ReadLine());
+Console.Write("Введите оценку студента с номером '2' -> ");
+int student2 = int.Parse(Console.ReadLine());
+Console.Write("Введите оценку студента с номером '3' -> ");
+int student3 = int.Parse(Console.ReadLine());
+Console.Write("Введите оценку студента с номером '4' -> ");
+int student4 = int.Parse(Console.ReadLine());
 
-if (result == 1)
+int[] students = [student1, student2, student3, student4];
+
+int max = students[0];
+int min = students[0];
+int counter = 0;
+int sum = 0;
+double result = 0;
+int success = 0;
+int failed = 0;
+
+foreach(var item in students)
 {
-    int r = num * num2;
-    Console.WriteLine(r);
+    counter++;
+    sum += item;
+    result = sum / counter;
+    if (item > max)
+    {
+        max = item;
+    }
+    if(item < min)
+    {
+        min = item;
+    }
+    if(item > 6)
+    {
+        success++;
+    }
+    if(item < 6)
+    {
+        failed++;
+    }
 }
-else if(result == 2)
-{
-     int r = num + num2;
-     Console.WriteLine(r);
-}
-else if (result == 3)
-{
-    int r = num - num2;
-    Console.WriteLine(r);
-}
-else if (result == 4)
-{
-    int r = num % num2;
-    Console.WriteLine(r);
-}
+Console.WriteLine("\n");
+Console.WriteLine("Средний балл -> " + result);
+Console.WriteLine("Максимальный балл -> " + max);
+Console.WriteLine("Минимальный балл -> " + min);
+Console.WriteLine("Сдали экзамен -> " + success);
+Console.WriteLine("НЕ сдали экзамен -> " + failed);
