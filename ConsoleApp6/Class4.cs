c2af095c7d3ca1d0b6a7cf5079dd10df00898896
itought <mountyjapan24@gmail.com>
2025-10-07 12:59:48 +0300

    Class

---
+++ ConsoleApp6/Class4.cs
@@ -0,0 +1,86 @@
+﻿
+//namespace fProj
+//{
+//    public class Point2D
+//    {
+//        public int PosX { get; private set; }
+//        public int PosY { get; private set; }
+//        public string Name { get; set; }
+
+
+
+//        public Point2D(int x, int y, string name)
+//        {
+//            PosX = x;
+//            PosY = y;
+//            Name = name;
+//        }
+
+
+//        public Point2D(int x, int y)
+//        {
+//            PosX = x;
+//            PosY = y;
+//            Name = "Dot";
+//        }
+
+//        public Point2D()
+//        {
+//            PosX = PosY = 0;
+//            Name = "Dot";
+//        }
+
+//        public void MoveTo(int x, int y)
+//        {
+//            PosX = x;
+//            PosY = y;
+//        }
+
+//        public void MoveTo(Point2D point)
+//        {
+//            PosX = point.PosX;
+//            PosY = point.PosY;
+//        }
+
+
+//        public string GetInfo()
+//        {
+//            return $"{Name}: {PosX} - {PosY}";
+//        }
+//    }
+//}
+
+
+using System;
+
+public class Planet
+{
+    public Planet()
+    {
+        public string Name { get; set; }
+    public string Distance { get; set; }
+    public string Size { get; set; }
+
+    public Planet(string name, string distance, string size)
+    {
+        Name = "Jupiter";
+        Distance = distance;
+        Size = size;
+    }
+    public Planet()
+    {
+        Size = "71492km";
+        Distance = "714m killometers"
+    }
+    public void RenameTo(string name, string distance, string size)
+    {
+        Name = name;
+        Distance = distance;
+        Size = size;
+    }
+    public string GetInfo()
+    {
+        return $"{Name}: {Distance} | {Size}";
+    }
+}
+}
--- ConsoleApp6/ConsoleApp6/Program.cs
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -1,102 +1,50 @@
-﻿Console.Write("Введите оценку студента с номером '1' -> ");
-int student1 = int.Parse(Console.ReadLine());
-Console.Write("Введите оценку студента с номером '2' -> ");
-int student2 = int.Parse(Console.ReadLine());
-Console.Write("Введите оценку студента с номером '3' -> ");
-int student3 = int.Parse(Console.ReadLine());
-Console.Write("Введите оценку студента с номером '4' -> ");
-int student4 = int.Parse(Console.ReadLine());
-
-int[] students = [student1, student2, student3, student4];
-
-int[] chetnie = [students.Length];
-
-int max = students[0];
-int min = students[0];
-int counter = 0;
-int sum = 0;
-double result = 0;
-
-for (int i = 0; i < students.Length; i++)
+﻿namespace fProj
 {
-    counter++;
-    Console.Write(students[i] + " | ");
-}
-
-void Sum()
-{
-    foreach (var item in students)
+    public class Point2D
     {
-        sum += item;
-    }
-    Console.WriteLine("\nСумма оценок -> " + sum);
-}
-void Max()
-{
-    foreach (var item in students)
-    {
-        if (item > max)
+        public int PosX { get; private set; }
+        public int PosY { get; private set; }
+        public string Name { get; set; }
+
+
+
+        public Point2D(int x, int y, string name)
         {
-            max = item;
+            PosX = x;
+            PosY = y;
+            Name = name;
         }
-        if (item < min)
-        {
-            min = item;
-        }
-    }
-    Console.WriteLine("Максимальная оценка -> " + max);
-    Console.WriteLine("Минимальная оценка -> " + min);
-}
-void Midle()
-{
-    foreach (var item in students)
-    {
-        result = sum / counter;
-    }
-    Console.WriteLine("Средняя оценка -> " + result);
 
-}
-void Chetnie()
-{
-    foreach (var item in students)
-    {
-        if (item % 2 == 0)
+
+        public Point2D(int x, int y)
         {
-            Console.Write(item + " | ");
+            PosX = x;
+            PosY = y;
+            Name = "Dot";
+        }
+
+        public Point2D()
+        {
+            PosX = PosY = 0;
+            Name = "Dot";
+        }
+
+        public void MoveTo(int x, int y)
+        {
+            PosX = x;
+            PosY = y;
+        }
+
+        public void MoveTo(Point2D point)
+        {
+            PosX = point.PosX;
+            PosY = point.PosY;
+        }
+
+
+        public string GetInfo()
+        {
+            return $"{Name}: {PosX} - {PosY}";
         }
     }
 }
-Sum();
-Max();
-Midle();
-Chetnie();
-
-
-//foreach(var item in students)
-//{
-//    counter++;
-//    sum += item;
-//    result = sum / counter;
-//    if (item > max)
-//    {
-//        max = item;
-//    }
-//    if(item < min)
-//    {
-//        min = item;
-//    }
-//    if(item > 6)
-//    {
-//        success++;
-//    }
-//    if(item < 6)
-//    {
-//        failed++;
-//    }
-//}
-//Console.WriteLine("\n");
-//Console.WriteLine("Средний балл -> " + result);
-//Console.WriteLine("Максимальный балл -> " + max);
-//Console.WriteLine("Минимальный балл -> " + min);
-//Console.WriteLine("Сдали экзамен -> " + success);
-//Console.WriteLine("НЕ сдали экзамен -> " + failed);
