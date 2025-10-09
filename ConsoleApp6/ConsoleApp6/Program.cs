99095d70fcb64e843f31c0e14f55e4473728e38e
itought <mountyjapan24@gmail.com>
2025-10-09 12:40:28 +0300

    daym1

--- ConsoleApp6/ConsoleApp6/Program.cs
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -1,7 +1,2 @@
-﻿using fProj;
-
-Planet Earth = new Planet("Earth", 12742, 150, "This planet has Life", "5 siptillion", "Moderate Weather", "Имеется", 1.225, 107000, 5, 7,"Earth");
-Earth.ResizeTo(399999);
-
-Earth.GetInfo();
+﻿Console.WriteLine("HEllo world");
 
